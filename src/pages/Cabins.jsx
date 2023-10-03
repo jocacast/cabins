1ce23d22e3e5c7067c11538843193fbc6762e51b
jocacast <jcastil91@gmail.com>
2023-10-03 10:32:30 -0400

    Side Bar styles, Header styles and API integration finished

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,17 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { useEffect } from "react";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(()=>{
+    getCabins().then((data)=>console.log(data));
+  },[])
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://ywsxckorykladsaflxqy.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"/>
     </Row>
   );
 }
